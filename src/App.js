664f368fc13789b07b019884411c85d946f52c1e
peter <mvonline@mvonline.com.br>
2022-04-08 22:43:32 -0300

    pronto v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { Container } from 'postcss';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="justify-items-stretch">
+          {/* <div class="grid grid-cols-6 gap-4 place-items-center overflow-x-auto">
+            <div class="col-start-2 col-span-4 flex mb-4">
+              <img src="milvest.svg"></img>
+
+            </div>
+            <div class="col-start-1 col-end-7 pt-8">
+            <Vesti nome="Tamires"></Vesti>
+
+            </div>
+            <div class="col-start-1 col-end-7">
+            <Vesti nome="Brenda"></Vesti>
+
+            </div>
+            <div class="col-start-1 col-end-7 ">
+            <Vesti nome="Fran"></Vesti>
+
+            </div>
+            <div class="col-start-1 col-end-7 ">
+            <Vesti nome="Catálogo"></Vesti>
+
+            </div>
+
+          </div> */}
+            <div class="flex justify-center mb-8">
+              <img src="milvest.svg"></img>
+
+            </div>
+            <div class="flex justify-center mb-4">
+            <Vesti nome="Tamires" link="https://wa.me/5511993703230"></Vesti>
+
+            </div>
+            <div class="flex justify-center mb-4">
+            <Vesti nome="Brenda" link="https://wa.me/5511987067219"></Vesti>
+
+            </div>
+            <div class="flex justify-center mb-4">
+            <Vesti nome="Fran" link="https://wa.me/5511987067140"></Vesti>
+
+            </div>
+            <div class="flex justify-center">
+            <Vesti nome="Catálogo" link="https://milvest.vesti.mobi/"></Vesti>
+
+            </div>
+
+
+          </div>
       </header>
     </div>
   );
 }
 
+function Whatsapp(props){
+  return(
+    <>
+    <button className='button-50'>{props.nome}</button>
+    </>
+  )
+}
+
+function Vesti(props){
+  return(
+    <>
+    <button className="button-51"><a href={props.link}>{props.nome}</a></button>
+    </>
+  )
+}
 export default App;
