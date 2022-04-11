24230457c06cb048e8e408cc4a6b582e12fcf3c5
peter <mvonline@mvonline.com.br>
2022-04-11 17:42:25 -0300

    catalogo link fix

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function Cata(props){
       <div class="flex justify-between">
         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"><path d="M5 7h13.79a2 2 0 0 1 1.99 2.199l-.6 6A2 2 0 0 1 18.19 17H8.64a2 2 0 0 1-1.962-1.608L5 7Z"/><path stroke-linecap="round" d="m5 7l-.81-3.243A1 1 0 0 0 3.22 3H2m6 18h2m6 0h2"/></g></svg>
         {props.nome}
-
+        {props.link}
       </div>
     </button>
     </>
